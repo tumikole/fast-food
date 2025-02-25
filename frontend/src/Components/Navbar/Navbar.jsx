59e07b0d82191797178bb4f82543d32a58e0e450
Samuel Ramollo <samuelramollo@MacBookPro.lan>
2025-02-26 01:42:17 +0200

    Modified about css

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,61 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [activeTab, setActiveTab] = useState("Home")
   return (
     <nav className="menu">
       <input type="checkbox" style={{ display: "none" }} id="check" />
-      <label htmlFor="check" className="checkbtn">
-      </label>
+      <label htmlFor="check" className="checkbtn"></label>
       <label className="logo">Olieven Kota & Grills</label>
       <ul>
-        <Link to="/">
-          <li style={{ display: "flex", gap: "1rem" }} className="active">
-            <div>
-              <box-icon name='home-alt-2' color='#ffffff' ></box-icon>
-            </div>
-            <div
-            >Home
-            </div>
-          </li>
-        </Link>
-        <Link to="/about">
-          <li style={{ display: "flex", gap: "1rem" }}>
-            <div>
-              <box-icon name='info-circle' color='#ffffff'></box-icon>
-            </div>
-            <div>About</div>
-          </li>
-        </Link>
-        <Link to="/how_it_works">
-          <li style={{ display: "flex", gap: "1rem" }}>
-            <div>
-              <box-icon name='briefcase' color='#ffffff' ></box-icon>
-            </div>
-            <div>How it works</div>
-          </li>
-        </Link>
-        <Link to="/menu">
-          <li style={{ display: "flex", gap: "1rem" }}>
-            <div><box-icon name='food-menu' color='#ffffff' ></box-icon></div>
-            <div>Menu</div>
-          </li></Link>
-        <Link to="/cart">
-          <li style={{ display: "flex", gap: "1rem" }}>
-            <div><box-icon name='cart' color='#ffffff' ></box-icon></div>
-            <div>Cart</div>
-          </li>
-        </Link>
-        <Link to="/review">
-          <li style={{ display: "flex", gap: "1rem" }}>
-            <div><box-icon name='low-vision' color='#ffffff' ></box-icon></div>
-            <div>Reviews</div>
-          </li>
-        </Link>
+        {[
+          { path: "/", name: "Home", icon: "home-alt-2" },
+          { path: "/about", name: "About", icon: "info-circle" },
+          { path: "/how_it_works", name: "How it works", icon: "briefcase" },
+          { path: "/menu", name: "Menu", icon: "food-menu" },
+          { path: "/cart", name: "Cart", icon: "cart" },
+          { path: "/review", name: "Reviews", icon: "low-vision" },
+        ].map((item, index) => (
+          <Link key={index} to={item.path}
+            onClick={() => setActiveTab(item.name)}
+            className={item.name === activeTab && "active"}
+          >
+            <li
+              style={{ display: "flex", gap: "1rem" }}
+            >
+              <div>
+                <box-icon name={item.icon} color="#ffffff"></box-icon>
+              </div>
+              <div>{item.name}</div>
+            </li>
+          </Link>
+        ))}
       </ul>
       <label htmlFor="check" className="closebtn">
-        <box-icon name='menu' animation='burst' color='#ffffff' ></box-icon>
+        <box-icon name="menu" animation="burst" color="#ffffff"></box-icon>
       </label>
     </nav>
   );
