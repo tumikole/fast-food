0fb8e84a7c1cecbdeedbf6572fff47b2e9f8a5b6
Samuel Ramollo <samuelramollo@MacBookPro.lan>
2025-02-25 16:51:48 +0200

    Added new designs

--- frontend/src/Components/LandingPage/LandingPage.jsx
+++ frontend/src/Components/LandingPage/LandingPage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import HeroSection from "../HeroSection/HeroSection";
-
+import HowItWorks from "../HowItWorks/HowItWorks";
+import About from "../About/About";
 import "./LandingPage.css";
 
 const LandingPage = () => {
