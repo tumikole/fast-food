60041cbb321cf01059fb5b31b8a91b0c52ddb6a3
Samuel Ramollo <samuelramollo@MacBookPro.lan>
2025-02-26 22:23:17 +0200

    Added functionality to add items to card

--- frontend/src/Components/LandingPage/LandingPage.jsx
+++ frontend/src/Components/LandingPage/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import HeroSection from "../HeroSection/HeroSection";
 import "./LandingPage.css";
+import Footer from "../Footer/Footer";
 
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <Footer />
     </div>
   );
 };
